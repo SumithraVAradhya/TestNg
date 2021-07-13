3c65348e43bf06938eb409ad1c015c9e3fb45ae4
SumithraVAradhya <60966160+SumithraVAradhya@users.noreply.github.com>
2021-07-14 00:49:25 +0530

    Create Listener.java

---
+++ Listener.java
@@ -0,0 +1,42 @@
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listener extends ScreenshotTest implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.println("Pass test case = "+iTestResult.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        ScreenshotTest.screenShot();
+        System.out.println("Failedtest case = "+iTestResult.getName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
