e3de9a4d56df03194763088e1740cf9191af7628
SumithraVAradhya <60966160+SumithraVAradhya@users.noreply.github.com>
2021-07-15 22:36:23 +0530

    Update Listener.java

--- Listener.java
+++ Listener.java
@@ -2,7 +2,7 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class Listener extends ScreenshotTest implements ITestListener {
+public class Listener extends Screenshot implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
